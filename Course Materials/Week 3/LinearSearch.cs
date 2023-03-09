a5afc35a5d465a25452a4500e90378343f6968b1
shokolade <juliama81@gmail.com>
2023-03-10 12:28:17 +1300

    Update and rename Project03.cs to LinearSearch.cs

---
+++ Course Materials/Week 3/LinearSearch.cs
@@ -0,0 +1,32 @@
+namespace LinerSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10] {10,20,30,40,50,60,70,80,90,100};
+      
+            Console.WriteLine("-------------------------");
+        
+       
+            Console.WriteLine("Enter the Search element\n");
+
+            string s = Console.ReadLine();
+            int x = Int32.Parse(s);
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (a[i] == x)
+                {
+                    Console.WriteLine("-------------------------");
+                    Console.WriteLine("Search successful");
+                    Console.WriteLine("Element {0} found at location {1}\n", x, i + 1);
+                    //return;
+                    Console.ReadLine();
+                }
+            }
+            Console.WriteLine("Entered element not found. Search unsuccessful");
+            Console.ReadLine();
+        }
+    }
+}
